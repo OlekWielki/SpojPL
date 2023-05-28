c79e1cf1d3ab1b4c582115851b2ad99c12dc02d4
OlekWielki <110064330+OlekWielki@users.noreply.github.com>
2023-05-28 18:10:46 +0200

    Update dwie-cyfry-silni.cpp

--- Łatwe/dwie-cyfry-silni.cpp
+++ Łatwe/dwie-cyfry-silni.cpp
@@ -1,31 +1,46 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm>
 using namespace std;
 int main(){
-    int n,a,b=0;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>a;
-    if(a==0)
-        cout<<"0"<<" "<<"1"<<endl;
-    else if(a==1)
-        cout<<"0"<<" "<<"1"<<endl;
-    else if(a==2)
-        cout<<"0"<<" "<<"2"<<endl;
-    else if(a==3)
-        cout<<"0"<<" "<<"6"<<endl;
-    else if(a==4)
-        cout<<"2"<<" "<<"4"<<endl;
-    else if(a==5)
-        cout<<"2"<<" "<<"0"<<endl;
-    else if(a==6)
-        cout<<"2"<<" "<<"0"<<endl;
-    else if(a==7)
-        cout<<"4"<<" "<<"0"<<endl;
-    else if(a==8)
-        cout<<"2"<<" "<<"0"<<endl;
-    else if(a==9)
-        cout<<"8"<<" "<<"0"<<endl;
-    else if(a>9)
-        cout<<"0"<<" "<<"0"<<endl;
-    }
+	int ile, co;
+	cin>>ile;
+	while(ile--){
+		cin>>co;
+		switch (co){
+			case 0:
+				cout<<"0 1"<<endl;
+				break;
+			case 1:
+				cout<<"0 1"<<endl;
+				break;
+			case 2:
+				cout<<"0 2"<<endl;
+				break;
+			case 3:
+				cout<<"0 6"<<endl;
+				break;
+			case 4:
+				cout<<"2 4"<<endl;
+				break;
+			case 5:
+				cout<<"2 0"<<endl;
+				break;
+			case 6:
+				cout<<"2 0"<<endl;
+				break;
+			case 7:
+				cout<<"4 0"<<endl;
+				break;
+			case 8:
+				cout<<"2 0"<<endl;
+				break;
+			case 9:
+				cout<<"8 0"<<endl;
+				break;
+			default:
+				cout<<"0 0"<<endl;
+		}
+	}
 }
